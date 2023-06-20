f324a86d94fcdfc384421964e9854598c89d7807
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-20 21:45:40 +0000

    prueba

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller ) {
+        "use strict";
+
+        return Controller.extend("prueba.preexamen.controller.Detail", {
+            onInit: function () {
+
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+  
+                oRouter.getRoute("detail").attachMatched(this._onRouteMatched, this);
+  
+            },
+  
+            _onRouteMatched: function (oEvent) {
+  
+                var oArgs, oView;
+  
+                oArgs = oEvent.getParameter("arguments");
+  
+                oView = this.getView();
+  
+                oView.bindElement({
+  
+                    path: "Customers('"+ oArgs.customerID + "')",
+  
+                    events: {
+  
+                        dataRequested: function () {
+  
+                            oView.setBusy(true);
+  
+                        },
+  
+                        dataReceived: function () {
+  
+                            oView.setBusy(false);
+  
+                        }
+  
+                    }
+  
+                });
+  
+            },
+        });
+    });
