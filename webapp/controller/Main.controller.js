815e6df88852bf5b432862bfbad1bc9a372fa330
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-20 20:54:16 +0000

    Define vista Main y su controller básico

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller ) {
         "use strict";
 
         return Controller.extend("prueba.preexamen.controller.Main", {
             onInit: function () {
 
+            },
+
+            onPress: function (oEvent){
+                var ID = oEvent.getSource().getBindingContext().getProperty("CustomerID")
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("detail", {
+                    customerID: ID
+                })
             }
         });
     });
