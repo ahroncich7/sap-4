a8e07103339ad659b71f238140174666f2b00cd1
Alejandro Hroncich <alehroncich@gmail.com>
2023-06-21 13:16:06 +0000

    Agrega filtro a la lista

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller ) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("prueba.preexamen.controller.Main", {
@@ -12,12 +15,28 @@ sap.ui.define([
 
             },
 
-            onPress: function (oEvent){
+            onPress: function (oEvent) {
                 var ID = oEvent.getSource().getBindingContext().getProperty("CustomerID")
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("detail", {
                     customerID: ID
                 })
+            },
+
+            handleSearch: function (oEvent) {
+                var filters = []
+                var query = oEvent.getParameter("newValue");
+                if (query && query.length > 0) {
+                    filters.push(new Filter({
+                        path: "ContactName",
+                        operator: FilterOperator.Contains,
+                        value1: query
+                    }));
+                }
+
+                var list = this.getView().byId("list");
+                var binding = list.getBinding("items");
+                binding.filter(filters);
             }
-        });
+        })
     });
